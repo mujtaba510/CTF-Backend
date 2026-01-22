fd323f40c5a11ef28771df03ef2cb7f3c349d523
Ghulam Mujtaba <gmujtaba1218@gmail.com>
2026-01-22 14:32:05 +0500

    add render link in swagger

--- config/email.ts
+++ config/email.ts
@@ -1,14 +1,32 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+// import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail', // or your email provider
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+// export default transporter;
+
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // or your email provider
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default transporter;
-
