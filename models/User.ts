977b4270d065e0de7f4ffd3536779d004280396e
Ghulam Mujtaba <gmujtaba1218@gmail.com>
2026-01-22 13:10:27 +0500

    ts to js

--- models/User.ts
+++ models/User.ts
@@ -1,21 +1,23 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Types } from "mongoose";
 
 export interface IUser extends Document {
-    username: string;
-    email: string;
-    universityName?: string;
-    phoneNumber?: string;
-    password: string;
-    otp?: string;
-    otpExpires?: Date;
-    isVerified: boolean;
-    role?: string;
-    isEligible?: boolean;
-    assignedChallenge?: number;
+  _id: Types.ObjectId;
+  username: string;
+  email: string;
+  universityName?: string;
+  phoneNumber?: string;
+  password: string;
+  otp?: string;
+  otpExpires?: Date;
+  isVerified: boolean;
+  role?: string;
+  isEligible?: boolean;
+  assignedChallenge?: number;
 }
 
-const userSchema = new mongoose.Schema<IUser>({
-    username : { type: String, required: true, unique: true },
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     universityName: { type: String, required: false },
     phoneNumber: { type: String, unique: true, required: true },
@@ -23,9 +25,11 @@ const userSchema = new mongoose.Schema<IUser>({
     otp: { type: String },
     otpExpires: { type: Date },
     isVerified: { type: Boolean, default: false },
-    role: { type: String, default: 'user' },
+    role: { type: String, default: "user" },
     isEligible: { type: Boolean, default: true },
     assignedChallenge: { type: Number, default: null },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
-export default mongoose.model<IUser>('User', userSchema);
+export default mongoose.model<IUser>("User", userSchema);
